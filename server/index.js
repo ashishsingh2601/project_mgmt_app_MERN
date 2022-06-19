c1a5804cef8b426b43e43ece3b1ce1822e52fa01
Ashish <as7220004@gmail.com>
2022-06-20 01:28:45 +0530

    Added: React app, fetch client and display functionality, spinner

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const colors = require('colors');
+const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require('./schema/schema.js')
@@ -9,9 +10,11 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-
 //connect to database
 connectDB();
+
+//passing in cors function to fix cors error
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
